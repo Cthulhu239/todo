a2b8b5d0bb8b73864e115514210ae5dca5e1d49b
Kushan Gajbe <kushangajbe73@gmail.com>
2025-07-28 19:51:27 +0530

    made it more functional

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.task_list, name='task-list'),      # /tasks/
-    path('add/', views.task_add, name='task-add'),    # /tasks/add/
-    path('delete/<int:id>/', views.task_delete, name='task-delete'),  # /tasks/delete/1/
+    path('task_list/', views.task_list, name='task_list'),
+    path('add/', views.task_add, name='task_add'),  # <-- Add this line
+    path('update/<int:task_id>/', views.update_task, name='update_task'),
+    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('delete/<int:task_id>/', views.task_delete, name='task_delete'),
 ]
+
+
+
--- todo/views.py
+++ todo/views.py
@@ -1,19 +1,58 @@
 from django.http import HttpResponse
 from .models import Task
-from django.shortcuts import redirect,render
+from django.shortcuts import  redirect, get_object_or_404,render
 
 
 
 def task_list(request):
-    tasks = Task.objects.all()  # gets all tasks from database
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+            return redirect('task_list')
+
+    tasks = Task.objects.all().order_by('-created')
     return render(request, 'todo/task_list.html', {'tasks': tasks})
 
-def task_add(request):
-    return HttpResponse("Add a task")
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
 
-def task_delete(request, id):
-    # Your logic to delete the task
-    # Example:
-    task = Task.objects.get(id=id)
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
     task.delete()
-    return redirect('some-view-name')
+    return redirect('task_list')
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        completed = request.POST.get('completed') == 'on'
+
+        if title:
+            task.title = title
+            task.completed = completed
+            task.save()
+        return redirect('task_list')
+
+    # GET request: Show the edit form inline for this task
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks, 'editing_task': task})
+ 
+
+def task_add(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+    return redirect('task_list')          
+
+
+
+
+      
+
+
